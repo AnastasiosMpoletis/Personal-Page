d7c5e72ea933b9dd5fc1330c3c7b3de19d5c6d91
Anastasios Mpoletis <anastasiosmpoletis@gmail.com>
2017-02-12 18:17:22 +0200

    Update anastasiosMpoletisJs.js

--- content/anastasiosMpoletisJs.js
+++ content/anastasiosMpoletisJs.js
@@ -1,51 +1,51 @@
 /* Smooth scrolling script */
 $(document).ready(function(){
- $(".innerLinks a, .navbar a, footer a[href='#index']").on('click', function(event) {
- if (this.hash !== "") {
-  event.preventDefault();
-  var hash = this.hash;
-  $('html, body').animate({ scrollTop: $(hash).offset().top }, 900, function(){ window.location.hash = hash; }); } 
- });
+  $(".innerLinks a, .navbar a, footer a[href='#index']").on('click', function(event) {
+  if (this.hash !== "") {
+    event.preventDefault();
+    var hash = this.hash;
+    $('html, body').animate({ scrollTop: $(hash).offset().top }, 900, function(){ window.location.hash = hash; }); } 
+  });
 })
 
 /* Display delay of content script */
 $(window).scroll(function() {
- $(".slideanim").each(function(){
-  var pos = $(this).offset().top;
-  var winTop = $(window).scrollTop();
-  if ( pos < winTop + (screen.height * 1.1) ) {
-   $(this).addClass("slide");
-  }
- });
+  $(".slideanim").each(function(){
+    var pos = $(this).offset().top;
+    var winTop = $(window).scrollTop();
+    if ( pos < winTop + (screen.height * 1.1) ) {
+      $(this).addClass("slide");
+    }
+  });
 });
 
 /* "To Top" button tooltip script */
 $(document).ready(function(){
- $('[data-toggle="tooltip"]').tooltip(); 
+  $('[data-toggle="tooltip"]').tooltip(); 
 });
 
 /* CV collapse glyphicon effect script */
 $('#demo').on('shown.bs.collapse', function () {
- if($(window).width() > 768) {
- $("#cvButton .glyphicon").toggleClass('rotate-180'); 
- }
+  if($(window).width() > 768) {
+    $("#cvButton .glyphicon").toggleClass('rotate-180'); 
+  }
 });
 $('#demo').on('hidden.bs.collapse', function () {
- if($(window).width() > 768) {
- $("#cvButton .glyphicon").toggleClass('rotate-180'); 
- }
+  if($(window).width() > 768) {
+    $("#cvButton .glyphicon").toggleClass('rotate-180'); 
+  }
 });
 
 /* Hover button shadow effect deactivate for mobile. Button shadow instead */ 
 $( window ).resize(function() {
- if($(window).width() < 768) {
-  $("#cvButton, #profilePhoto, .thumbButton, .img-thumbnail").removeClass("boxShadowEffect");
-  $("#linkedInButton, .gitHub").removeClass("buttonHover");
-  $("#cvButton, #linkedInButton, .thumbButton, .gitHub").addClass("boxShadow");
- }
- else {
-  $("#cvButton, #linkedInButton, .thumbButton, .gitHub").removeClass("boxShadow");
-  $("#cvButton, #profilePhoto, .thumbButton, .img-thumbnail").addClass("boxShadowEffect"); 
-  $("#linkedInButton, .gitHub").addClass("buttonHover");
- }
+  if($(window).width() < 768) {
+    $("#cvButton, #profilePhoto, .portfolioLinkDiv, .img-thumbnail").removeClass("boxShadowEffect");
+    $("#cvButton, #linkedInButton, .portfolioLinkDiv, .gitHub").addClass("boxShadow");
+    $("#linkedInButton, .gitHub").removeClass("buttonHover");
+  }
+  else {
+    $("#cvButton, #linkedInButton, .portfolioLinkDiv, .gitHub").removeClass("boxShadow");
+    $("#cvButton, #profilePhoto, .portfolioLinkDiv, .img-thumbnail").addClass("boxShadowEffect"); 
+    $("#linkedInButton, .gitHub").addClass("buttonHover");
+  }
 });
